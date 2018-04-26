d4fa410f8b454f8020dc88209b425016c1012afb
ShadowNigga <mstevennlopez@gmail.com>
2018-04-26 15:25:42 -0500

    Mapeo Entity to DTO and DTO to Entity for one and more of one dates (ProductoDTO, MarcaDTO,TipoProductoDTO)

--- MarketPlaceKonrad/src/main/java/co/edu/konrad/MarketPlaceKonrad/dto/MarcaDTO.java
+++ MarketPlaceKonrad/src/main/java/co/edu/konrad/MarketPlaceKonrad/dto/MarcaDTO.java
@@ -6,19 +6,63 @@
 package co.edu.konrad.MarketPlaceKonrad.dto;
 
 import co.edu.konrad.MarketPlaceKonrad.entities.MarcaEntity;
+import java.util.ArrayList;
+import java.util.List;
 
 /**
+ * Mapeo Entidades a DTO'S y DTO'S a entidades para Marca
  *
  * @author Manuel S. Nino
  */
 class MarcaDTO {
-    
+
+    // Variables tipo DTO de la Marca
     private Long codMarca;
-    
+
     private String nomMarca;
-    
-    public MarcaDTO(MarcaEntity marca){
-    this.codMarca = marca.getCodMarca();
-    this.nomMarca = marca.getNomMarca();
+
+    //Mapeo MarcaEntity a MarcaDTO
+    public MarcaDTO(MarcaEntity marca) {
+        this.codMarca = marca.getCodMarca();
+        this.nomMarca = marca.getNomMarca();
+    }
+
+    //MarcaDTO a MarcaEntity
+    public MarcaEntity toEntity() {
+        MarcaEntity marca = new MarcaEntity();
+        marca.setCodMarca(this.codMarca);
+        marca.setNomMarca(this.nomMarca);
+        return marca;
+    }
+
+    /**
+     * Mapeo para el caso más de una MarcaEntity a MarcaDTO
+     *
+     * @return listaMarcaDTO
+     */
+    public List<MarcaDTO> toMarcaList(List<MarcaEntity> marcaList) {
+        List<MarcaDTO> listaMarcaDTO = new ArrayList<>();
+        for (int i = 0; i < marcaList.size(); i++) {
+            listaMarcaDTO.add(new MarcaDTO(marcaList.get(i)));
+        }
+
+        return listaMarcaDTO;
+    }
+
+    // Getter and Settter Para poder recibir y enviar los datos de MarcaDTO
+    public Long getCodMarca() {
+        return codMarca;
+    }
+
+    public void setCodMarca(Long codMarca) {
+        this.codMarca = codMarca;
+    }
+
+    public String getNomMarca() {
+        return nomMarca;
+    }
+
+    public void setNomMarca(String nomMarca) {
+        this.nomMarca = nomMarca;
     }
 }
