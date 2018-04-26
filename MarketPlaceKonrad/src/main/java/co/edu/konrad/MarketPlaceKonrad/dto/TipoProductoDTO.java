741fcc02b66cca208801d6162986d216df7b1947
ShadowNigga <mstevennlopez@gmail.com>
2018-04-26 15:25:48 -0500

    Mapeo Entity to DTO and DTO to Entity for one and more of one dates (ProductoDTO, MarcaDTO,TipoProductoDTO)

--- MarketPlaceKonrad/src/main/java/co/edu/konrad/MarketPlaceKonrad/dto/TipoProductoDTO.java
+++ MarketPlaceKonrad/src/main/java/co/edu/konrad/MarketPlaceKonrad/dto/TipoProductoDTO.java
@@ -6,22 +6,83 @@
 package co.edu.konrad.MarketPlaceKonrad.dto;
 
 import co.edu.konrad.MarketPlaceKonrad.entities.TipoProductoEntity;
+import java.util.ArrayList;
+import java.util.List;
 
 /**
+ * Mapeo Entidades a DTO'S y DTO'S a entidades para TipoProducto
  *
- * @author Juan
+ * @author Manuel S. Nino
  */
 class TipoProductoDTO {
-    
+    // Variables tipo DTO para TipoProducto
+
     private Long codTipoProducto;
-    
+
     private String nomTipoProducto;
-    
+
     private String descripcion;
-    
-    public TipoProductoDTO (TipoProductoEntity tipoProducto){
-    this.codTipoProducto = tipoProducto.getCodTipo();
-    this.nomTipoProducto = tipoProducto.getNomTipoProducto();
-    this.descripcion = tipoProducto.getDescripcion();
+
+    /**
+     * Mapeo TipoProductoEntity a TipoProductoDTO
+     *
+     * @param tipoProducto
+     */
+    public TipoProductoDTO(TipoProductoEntity tipoProducto) {
+        this.codTipoProducto = tipoProducto.getCodTipoProducto();
+        this.nomTipoProducto = tipoProducto.getNomTipoProducto();
+        this.descripcion = tipoProducto.getDescripcion();
+    }
+
+    /**
+     * Mapeo TipoProductoDTO a TipoProductoEntitu
+     *
+     * @return TipoProductoEntity
+     */
+    public TipoProductoEntity toEntity() {
+        TipoProductoEntity tipoProducto = new TipoProductoEntity();
+        tipoProducto.setCodTipoProducto(this.codTipoProducto);
+        tipoProducto.setNomTipoProducto(this.nomTipoProducto);
+        tipoProducto.setDescripcion(this.descripcion);
+        return tipoProducto;
+    }
+
+    /**
+     * Mapeo para el caso de más de un TipoProductoEntity a TipoProductoDTO
+     *
+     * @param tipoProductoList
+     * @return listaTipoProductoDTo
+     */
+    public List<TipoProductoDTO> toTipoProductoList(List<TipoProductoEntity> tipoProductoList) {
+        List<TipoProductoDTO> listaTipoProductoDTo = new ArrayList<>();
+        for (int i = 0; i < tipoProductoList.size(); i++) {
+            listaTipoProductoDTo.add(new TipoProductoDTO(tipoProductoList.get(i)));
+        }
+        return listaTipoProductoDTo;
+    }
+
+    // Getter and Settter Para poder recibir y enviar los datos de TipoProductoDTO
+    public Long getCodTipoProducto() {
+        return codTipoProducto;
+    }
+
+    public void setCodTipoProducto(Long codTipoProducto) {
+        this.codTipoProducto = codTipoProducto;
+    }
+
+    public String getNomTipoProducto() {
+        return nomTipoProducto;
+    }
+
+    public void setNomTipoProducto(String nomTipoProducto) {
+        this.nomTipoProducto = nomTipoProducto;
+    }
+
+    public String getDescripcion() {
+        return descripcion;
+    }
+
+    public void setDescripcion(String descripcion) {
+        this.descripcion = descripcion;
     }
 }
