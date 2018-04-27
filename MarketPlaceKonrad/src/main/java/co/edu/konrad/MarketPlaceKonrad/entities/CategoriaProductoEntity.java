f274c682954d8ebd12839ab113f3927666761e46
Maria <mjrodriguez0512@gmail.com>
2018-04-26 21:44:02 -0500

    CategoriaProductoDTO, CategoriaDTO

--- MarketPlaceKonrad/src/main/java/co/edu/konrad/MarketPlaceKonrad/entities/CategoriaProductoEntity.java
+++ MarketPlaceKonrad/src/main/java/co/edu/konrad/MarketPlaceKonrad/entities/CategoriaProductoEntity.java
@@ -4,6 +4,7 @@
  * and open the template in the editor.
  */
 package co.edu.konrad.MarketPlaceKonrad.entities;
+
 import java.io.Serializable;
 import javax.persistence.Entity;
 import javax.persistence.Id;
@@ -16,30 +17,53 @@ import javax.persistence.JoinColumn;
  *
  * @author Manuel S. Nino
  */
-@Entity (name = "Categoria_producto")
-public class CategoriaProductoEntity implements Serializable{
+@Entity(name = "Categoria_producto")
+public class CategoriaProductoEntity implements Serializable {
+
     /**
      * Controlador de versiones
-     * 
+     *
      */
     private final static long serialVersionUID = 1L;
-    
+
     @Id
-    @Column (name = "cod_categoriaProducto")
+    @Column(name = "cod_categoriaProducto")
     private Long codCategoriaProducto;
     /**
-     * 
+     *
      * Relacion con categoria
      */
     @ManyToOne
-    @JoinColumn  (name = "cod_categoria")
+    @JoinColumn(name = "cod_categoria")
     private CategoriaEntity codCategoria;
     /**
      * Relacion con producto
      */
     @ManyToOne
-    @JoinColumn (name = "cod_producto")
+    @JoinColumn(name = "cod_producto")
     private ProductoEntity producto;
-    
- 
+
+    public Long getCodCategoriaProducto() {
+        return codCategoriaProducto;
+    }
+
+    public void setCodCategoriaProducto(Long codCategoriaProducto) {
+        this.codCategoriaProducto = codCategoriaProducto;
+    }
+
+    public CategoriaEntity getCodCategoria() {
+        return codCategoria;
+    }
+
+    public void setCodCategoria(CategoriaEntity codCategoria) {
+        this.codCategoria = codCategoria;
+    }
+
+    public ProductoEntity getProducto() {
+        return producto;
+    }
+
+    public void setProducto(ProductoEntity producto) {
+        this.producto = producto;
+    }
 }
